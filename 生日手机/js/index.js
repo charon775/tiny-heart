d1276c3d46997b97466e73fb3a4379de819486d8
charon775 <82213895+charon775@users.noreply.github.com>
2021-04-30 15:55:19 +0800

    Update and rename birthday-mobile/js/index.js to 生日手机/js/index.js

--- 生日手机/js/index.js
+++ 生日手机/js/index.js
@@ -2,8 +2,8 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="123" &&  pwd=="123"){
-			event.preventDefault();
+if(userName=="roise" &&  pwd=="0428"){
+	event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
 			setTimeout(function(){location.href="BirthdayCake.html";},2000);
